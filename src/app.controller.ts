427ea5a51e4877dab1dff2fdd4d4a66ca0daf64f
Arsenii <origingoy@gmail.com>
2024-04-09 18:35:30 +0300

    set health-check endpoint to public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from './auth/decorators/public.decorator';
 
 @Controller()
 @ApiTags('base')
 export class AppController {
   @Get('health-check')
+  @Public()
   getHello(): { ok: boolean } {
     return { ok: true };
   }
