941f10cb13704cb9b1f83283feda0755b70fa0c2
Arsenii <origingoy@gmail.com>
2024-04-09 18:36:50 +0300

    better logging

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,18 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-import { env } from '../env';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { NodeEnv, env } from '../env';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    const logOptions: Prisma.LogLevel[] = ['info', 'warn', 'error'];
+    if (env.NODE_ENV === NodeEnv.local) {
+      logOptions.push('query');
+    }
+    super({ log: logOptions });
+  }
 
   async onModuleInit() {
     this.logger.log(`Initializing Prisma with db: ${env.DATABASE_URL}`);
