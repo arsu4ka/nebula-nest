c3fd50dabc4b1c4975fc2feaa9e697dbbc8eb14d
Arsenii <origingoy@gmail.com>
2024-04-09 18:35:37 +0300

    added logs to prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,13 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { env } from '../env';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
   async onModuleInit() {
+    this.logger.log(`Initializing Prisma with db: ${env.DATABASE_URL}`);
     await this.$connect();
   }
 }
